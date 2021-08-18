345476ffbf84ba0c0b632c6a390411b0fdd653da
artem <artemuskov92@gmail.com>
2021-08-18 12:43:46 +0300

    google http

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const app = require('./app')
 const port = process.env.PORT || 5000
-const https = require('https')
-const fs = require('fs')
-const path = require('path')
-const httpsOptions = {
-    cert: fs.readFileSync(path.join(__dirname,'ssl','itstart_in_ua.crt')),
-    key: fs.readFileSync(path.join(__dirname,'ssl','itstart_in_ua.key'))
-  }
-  https.createServer(httpsOptions,app).listen(port,()=>{
+
+
+app.listen(port, ()=>{
     console.log(`server starterd on ${port}`)
-  })
-// app.listen(port, ()=>{
-//     console.log(`server starterd on ${port}`)
-// })
+})
