a2e117f49765fdf85bed0cd8a47bce5c5053e7d2
artem <artemuskov92@gmail.com>
2021-08-30 22:50:43 +0300

    https

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ if(process.env.NODE_ENV === 'production'){
        __dirname,'client','dist','it-start','index.html'
      )
    )
+   res.redirect('https://' + req.headers.host + req.url);
  })
 }
 if(process.env.NODE_ENV === 'production'){
@@ -80,7 +81,6 @@ if(process.env.NODE_ENV === 'production'){
   })
   http.createServer( function(req,res){
     res.writeHead(301,{"location":"https://"+req.headers['host']+req.url})
-    res.redirect('https://' + req.headers.host + req.url);
     res.end()
   }).listen(80)
 }else if(process.env.NODE_ENV === "developmant"){
