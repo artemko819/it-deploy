02a4fd197e8649dec7cc85a6c959180f2250ee0e
artem <artemuskov92@gmail.com>
2021-09-01 21:04:30 +0300

    https

--- client/src/app/quiz/quiz-rezult-page/quiz-rezult-page.component.ts
+++ client/src/app/quiz/quiz-rezult-page/quiz-rezult-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-quiz-rezult-page',
@@ -6,11 +7,42 @@ import {Component, OnInit, } from '@angular/core';
   styleUrls: ['./quiz-rezult-page.component.css']
 })
 export class QuizRezultPageComponent implements OnInit {
- 
+  private subscription: Subscription;
+
+  public dateNow = new Date();
+  public dDay = new Date('Sep 28 2021 09:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference;
+  public secondsToDday;
+  public minutesToDday;
+  public hoursToDday;
+  public daysToDday;
+
+  private getTimeDifference () {
+    this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+    this.allocateTimeUnits(this.timeDifference);
+  }
+
+  private allocateTimeUnits (timeDifference) {
+    this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+    this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+    this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+}
+
   constructor( ) { }
 
   ngOnInit(): void {
-    // window.location.reload();
+    this.subscription = interval(1000)
+    .subscribe(x => { this.getTimeDifference(); });
   }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
 
 }
